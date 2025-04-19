e2d7b8e31afc78afacf5bd84f3b1d935152d1405
Nicolas Cazal <nicolas.cazal02@gmail.com>
2025-04-19 03:17:02 -0300

    version que pasa las pruebas

--- shell/builtin.c
+++ shell/builtin.c
@@ -1,49 +1,63 @@
 #include "builtin.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <errno.h>
 
-// returns true if the 'exit' call
-// should be performed
-//
-// (It must not be called from here)
+#define COMANDO_NO_CORRESPONDE 0
+
 int
 exit_shell(char *cmd)
 {
-	// Your code here
+    if (strncmp(cmd, "exit", 4) != 0
+        || (cmd[4] != '\0' && cmd[4] != ' '))           
+        return COMANDO_NO_CORRESPONDE;
 
-	return 0;
+    exit(EXIT_SUCCESS);
 }
 
-// returns true if "chdir" was performed
-//  this means that if 'cmd' contains:
-// 	1. $ cd directory (change to 'directory')
-// 	2. $ cd (change to $HOME)
-//  it has to be executed and then return true
-//
-//  Remember to update the 'prompt' with the
-//  	new directory.
-//
-// Examples:
-//  1. cmd = ['c','d', ' ', '/', 'b', 'i', 'n', '\0']
-//  2. cmd = ['c','d', '\0']
 int
 cd(char *cmd)
 {
-	// Your code here
+    if (strncmp(cmd, "cd", 2) != 0
+        || (cmd[2] != '\0' && cmd[2] != ' '))           
+        return COMANDO_NO_CORRESPONDE;
 
-	return 0;
+    char *arg = cmd + 2;
+    while (*arg == ' ') arg++;                     
+
+    if (*arg == '\0') {                            
+        arg = getenv("HOME");
+        if (!arg) {
+            perror("cd: no se pudo obtener el directorio HOME");
+            return 1;
+        }
+    }
+
+    if (chdir(arg) == -1) {                        
+        perror("cd: no se pudo cambiar de directorio");
+    }
+    return 1;
 }
 
-// returns true if 'pwd' was invoked
-// in the command line
-//
-// (It has to be executed here and then
-// 	return true)
 int
 pwd(char *cmd)
 {
-	// Your code here
+	if (strncmp(cmd, "pwd", 3) != 0
+	|| (cmd[3] != '\0' && cmd[3] != ' '))           
+	return COMANDO_NO_CORRESPONDE;
 
-	return 0;
+    char cwd[PATH_MAX];
+    if (getcwd(cwd, sizeof(cwd)))
+        printf("%s\n", cwd);
+    else
+        perror("pwd: no se pudo obtener el directorio actual");
+    return 1;
 }
+ 
+
 
 // returns true if `history` was invoked
 // in the command line
