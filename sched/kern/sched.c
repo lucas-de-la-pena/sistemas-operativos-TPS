ab9d9cff4e80e6467966e44c82e027e52e1eb24f
benjaandruet <bandruet@fi.uba.ar>
2025-05-17 01:20:41 -0300

    Se modifico el sched para que cumpla con el formato clang

--- sched/kern/sched.c
+++ sched/kern/sched.c
@@ -38,9 +38,10 @@ sched_yield(void)
 			env_run(&envs[idx]);
 		}
 	}
-	if (curenv && curenv->env_status == ENV_RUNNING && curenv->env_cpunum == thiscpu->cpu_id) {
-		env_run(curenv);
-	}
++	if (curenv && curenv->env_status == ENV_RUNNING &&
++	    curenv->env_cpunum == thiscpu->cpu_id) {
+ 		env_run(curenv);
+ 	}
 
 	// No hay entornos para correr, así que detenemos la CPU
 	sched_halt();
