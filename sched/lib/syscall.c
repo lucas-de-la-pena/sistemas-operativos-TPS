b7ee3cfee6816f30c032407c4f7706e05e789477
tvillegas98 <tvillegas@fi.uba.ar>
2025-05-18 12:41:44 -0300

    feat: add get and set priories, also set the priority in fork

--- sched/lib/syscall.c
+++ sched/lib/syscall.c
@@ -112,3 +112,13 @@ sys_ipc_recv(void *dstva)
 {
 	return syscall(SYS_ipc_recv, 1, (uint32_t) dstva, 0, 0, 0, 0);
 }
+
+int sys_get_priority(envid_t envid)
+{
+	return syscall(SYS_get_priority, 1, envid, 0, 0, 0, 0);
+}
+
+int sys_set_priority(envid_t envid, int priority)
+{
+	return syscall(SYS_set_priority, 1, envid, priority, 0, 0, 0);
+}
