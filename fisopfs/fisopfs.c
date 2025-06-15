92f65533aec87bb4e610935cf292a56d8ba08276
tvillegas98 <tvillegas@fi.uba.ar>
2025-06-15 12:22:56 -0300

    code: clang format

--- fisopfs/fisopfs.c
+++ fisopfs/fisopfs.c
@@ -13,7 +13,9 @@ fisopfs_save_fs_on_exit()
 
 	FILE *archivo_disco = fopen(nombre_archivo_disco, "w");
 	if (!archivo_disco) {
-		fprintf(stderr, "[Debug] ERROR al guardar fisop: %s\n", strerror(errno));
+		fprintf(stderr,
+		        "[Debug] ERROR al guardar fisop: %s\n",
+		        strerror(errno));
 		return;
 	}
 
@@ -90,9 +92,9 @@ fisopfs_getattr(const char *path, struct stat *st)
 static int
 fisopfs_getdir(const char *path, fuse_dirh_t h, fuse_dirfil_t filler)
 {
-    printf("[debug] fisopfs_getdir - ruta: %s\n", path);
+	printf("[debug] fisopfs_getdir - ruta: %s\n", path);
 
-    filler(h, ".", 0);
+	filler(h, ".", 0);
 	filler(h, "..", 0);
 
 	struct inode *inodo_dir;
@@ -101,10 +103,10 @@ fisopfs_getdir(const char *path, fuse_dirh_t h, fuse_dirfil_t filler)
 		return err;
 	for (int i = 0; i < MAX_INODES; i++) {
 		if (super_b.bitmap_inodes[i] &&
-			strcmp(super_b.inodes[i].directory_path, inodo_dir->path) == 0) {
-
-			const char* child_full_path = super_b.inodes[i].path;
-			const char* child_name = strrchr(child_full_path, '/');
+		    strcmp(super_b.inodes[i].directory_path, inodo_dir->path) ==
+		            0) {
+			const char *child_full_path = super_b.inodes[i].path;
+			const char *child_name = strrchr(child_full_path, '/');
 
 			if (child_name == NULL) {
 				filler(h, child_full_path, 0);
@@ -118,10 +120,7 @@ fisopfs_getdir(const char *path, fuse_dirh_t h, fuse_dirfil_t filler)
 }
 
 static int
-fisopfs_read(const char *path,
-             char *buffer,
-             size_t size,
-             off_t offset)
+fisopfs_read(const char *path, char *buffer, size_t size, off_t offset)
 {
 	printf("[debug] fisopfs_read - ruta: %s, offset: %lu, tamaño: %lu\n",
 	       path,
@@ -140,10 +139,7 @@ fisopfs_read(const char *path,
 }
 
 static int
-fisopfs_write(const char *path,
-              const char *buf,
-              size_t size,
-              off_t offset)
+fisopfs_write(const char *path, const char *buf, size_t size, off_t offset)
 {
 	printf("[Debug] fisopfs_write: %s, offset: %lu, tamaño: %lu\n",
 	       path,
@@ -155,7 +151,7 @@ fisopfs_write(const char *path,
 static int
 fisopfs_create(const char *path, mode_t mode, dev_t dev)
 {
-    (void)dev;
+	(void) dev;
 	printf("[Debug] fisop_touch (crear/mknod): %s\n", path);
 	return create_file(path, mode);
 }
@@ -215,7 +211,7 @@ fisopfs_utime(const char *path, struct utimbuf *ub)
 }
 
 static struct fuse_operations operations = {
-    .getattr = fisopfs_getattr,
+	.getattr = fisopfs_getattr,
 	.getdir = fisopfs_getdir,
 	.read = fisopfs_read,
 	.write = fisopfs_write,
@@ -230,51 +226,55 @@ static struct fuse_operations operations = {
 int
 main(int argc, char *argv[])
 {
-    char temp_disk_path[MAX_PATH] = DEFAULT_FILE_DISK;
-    for (int i = 1; i < argc; i++) {
-        if (i + 1 < argc && strcmp(argv[i], "--filedisk") == 0) {
-            strncpy(temp_disk_path, argv[i + 1], MAX_PATH - 1);
+	char temp_disk_path[MAX_PATH] = DEFAULT_FILE_DISK;
+	for (int i = 1; i < argc; i++) {
+		if (i + 1 < argc && strcmp(argv[i], "--filedisk") == 0) {
+			strncpy(temp_disk_path, argv[i + 1], MAX_PATH - 1);
 
-            for (int j = i; j < argc - 2; j++) {
-                argv[j] = argv[j + 2];
-            }
-            argc -= 2;
-            break;
-        }
-    }
+			for (int j = i; j < argc - 2; j++) {
+				argv[j] = argv[j + 2];
+			}
+			argc -= 2;
+			break;
+		}
+	}
 
-    if (temp_disk_path[0] != '/') {
-        char current_dir[MAX_PATH];
-        if (getcwd(current_dir, sizeof(current_dir)) != NULL) {
-            if (strlen(current_dir) + 1 + strlen(temp_disk_path) + 1 > MAX_PATH) {
-                fprintf(stderr, "Error: La ruta absoluta al archivo de disco es demasiado larga.\n");
-                return 1;
-            }
-            strcpy(nombre_archivo_disco, current_dir);
-            strcat(nombre_archivo_disco, "/");
-            strcat(nombre_archivo_disco, temp_disk_path);
-        } else {
-            perror("Error obteniendo el directorio actual con getcwd");
-            return 1;
-        }
-    } else {
-        strcpy(nombre_archivo_disco, temp_disk_path);
-    }
+	if (temp_disk_path[0] != '/') {
+		char current_dir[MAX_PATH];
+		if (getcwd(current_dir, sizeof(current_dir)) != NULL) {
+			if (strlen(current_dir) + 1 + strlen(temp_disk_path) + 1 >
+			    MAX_PATH) {
+				fprintf(stderr, "Error: La ruta absoluta al archivo de disco es demasiado larga.\n");
+				return 1;
+			}
+			strcpy(nombre_archivo_disco, current_dir);
+			strcat(nombre_archivo_disco, "/");
+			strcat(nombre_archivo_disco, temp_disk_path);
+		} else {
+			perror("Error obteniendo el directorio actual con "
+			       "getcwd");
+			return 1;
+		}
+	} else {
+		strcpy(nombre_archivo_disco, temp_disk_path);
+	}
 
-    printf("init - cargando el estado del sistema de archivos desde la ruta absoluta: %s\n", nombre_archivo_disco);
+	printf("init - cargando el estado del sistema de archivos desde la "
+	       "ruta absoluta: %s\n",
+	       nombre_archivo_disco);
 
-    FILE *archivo_disco = fopen(nombre_archivo_disco, "r");
-    if (!archivo_disco) {
-        initialize_fs();
-    } else {
-        if (fread(&super_b, sizeof(super_b), 1, archivo_disco) != 1) {
-            fprintf(stderr, "Error al leer el superbloque. Se inicializa un nuevo FS.\n");
-            initialize_fs();
-        }
-        fclose(archivo_disco);
-    }
+	FILE *archivo_disco = fopen(nombre_archivo_disco, "r");
+	if (!archivo_disco) {
+		initialize_fs();
+	} else {
+		if (fread(&super_b, sizeof(super_b), 1, archivo_disco) != 1) {
+			fprintf(stderr, "Error al leer el superbloque. Se inicializa un nuevo FS.\n");
+			initialize_fs();
+		}
+		fclose(archivo_disco);
+	}
 
-    atexit(fisopfs_save_fs_on_exit);
+	atexit(fisopfs_save_fs_on_exit);
 
-    return fuse_main(argc, argv, &operations);
+	return fuse_main(argc, argv, &operations);
 }
